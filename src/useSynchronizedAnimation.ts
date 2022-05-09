951208f05825713c2bf74f5c4dba2bb04e1cac45
Gustav Tiger <gustav.tiger@axis.com>
2022-05-09 14:01:25 +0200

    fix: correct types for useSynchronizedAnimation (#16)
    If we don't default to null it is not usable with styled-components.
    Also add template types similar to useRef.

--- src/useSynchronizedAnimation.ts
+++ src/useSynchronizedAnimation.ts
@@ -1,11 +1,14 @@
 import { useLayoutEffect, useRef } from 'react'
 
-export const useSynchronizedAnimation = (options?: GetAnimationsOptions) => {
-  const ref = useRef<HTMLElement>()
+export const useSynchronizedAnimation = <E extends HTMLElement>(
+  options?: GetAnimationsOptions
+) => {
+  const ref = useRef<E | null>(null)
   useLayoutEffect(() => {
+    const currentTime = document.timeline?.currentTime ?? 0
     ref.current
       ?.getAnimations(options)
-      .forEach(a => (a.currentTime = document.timeline.currentTime))
+      .forEach(a => (a.currentTime = currentTime))
   }, [options])
   return ref
 }
